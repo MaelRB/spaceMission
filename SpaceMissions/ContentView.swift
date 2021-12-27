a9144e89810c9fa2a939918dc8f9524ce043d814
MaëlRB <mael.rb@outlook.com>
2021-12-27 15:04:32 +0100

    Initial Commit

---
+++ SpaceMissions/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SpaceMissions
+//
+//  Created by Mael Romanin Bluteau on 27/12/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SpaceMissions/SpaceMissionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceMissionsApp.swift
+//  SpaceMissions
+//
+//  Created by Mael Romanin Bluteau on 27/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct SpaceMissionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
